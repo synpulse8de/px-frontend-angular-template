7bf773db15ebea202e6191c31fdea5154622e61c
DennisMac <dennis.weller@profect.de>
2023-10-04 15:23:06 +0200

    removed unused files

--- src/app/{% if store and store_example %}features{% endif %}/example-feature-2/pages/ex2-base-page/ex2-base-page.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core'
-
-@Component({
-  selector: 'app-ex2-base-page',
-  templateUrl: './ex2-base-page.component.html',
-  styleUrls: ['./ex2-base-page.component.scss'],
-})
-export class Ex2BasePageComponent {}
