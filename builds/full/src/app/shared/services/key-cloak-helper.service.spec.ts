73978389d69a7549e6f54e29ecce7fca10970ced
Dennis Weller <dennis.weller@synpulse8.com>
2024-01-15 17:18:34 +0100

    added correct structure for kustomize added placeholder for devspace profiles fixed missing test service definition

--- builds/full/src/app/shared/services/key-cloak-helper.service.spec.ts
+++ builds/full/src/app/shared/services/key-cloak-helper.service.spec.ts
@@ -1,12 +1,23 @@
 import { TestBed } from '@angular/core/testing'
 
 import { KeyCloakHelperService } from './key-cloak-helper.service'
+import { KeycloakService } from 'keycloak-angular'
 
 describe('KeyCloakHelperService', () => {
   let service: KeyCloakHelperService
 
   beforeEach(() => {
-    TestBed.configureTestingModule({})
+    service = jasmine.createSpyObj('KeycloakService', [
+      'loadUserProfile',
+      'getKeycloakInstance',
+    ])
+    TestBed.configureTestingModule({
+      providers: [
+        KeyCloakHelperService,
+        { provide: KeycloakService, useValue: service },
+      ],
+    })
+
     service = TestBed.inject(KeyCloakHelperService)
   })
 
